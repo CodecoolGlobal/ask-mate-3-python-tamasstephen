588dfdbe43082d4202a2b3973900bb96aee8e814
tamasstephen <kicsitnagyon@gmail.com>
2021-08-02 11:31:21 +0200

    Add function open_questions

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def open_questions():
+    questions= ["return value of a function"] 
+    return render_template("index.html", questions=questions) 
+
+
+@app.route("question/<question_id>")
+def open_question_page(question_id):
+    question_page_content = ["return value of a func with the question id arg"]
+    return render_template("question.html", content=question_page_content)
 
 
 if __name__ == "__main__":
