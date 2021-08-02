58ff4406f7f54d81e966340e97b67619a03f8c0a
tamasstephen <kicsitnagyon@gmail.com>
2021-08-02 18:35:29 +0200

    Add function add_question

--- connection.py
+++ connection.py
@@ -1,7 +1,19 @@
 import csv
 
 
-def read_data_from_file(filename="sample_data/question.csv"):
+
+
+def read_data_from_file(filename="sample_data/test_questions.csv"):
     with open(filename, "r") as csvfile:
         reader = csv.DictReader(csvfile)
         return list(reader)
+
+
+def write_data_to_file(data, headers, filename="sample_data/test_questions.csv"):
+    with open(filename, "w") as csvfile:
+        fieldnames = headers
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for fields in data:
+            writer.writerow(fields)
+         
--- data_handler.py
+++ data_handler.py
@@ -1,24 +1,25 @@
-# from datetime import datetime
+from time import time
+import math
 import connection
 
-HEADERS = ["id", "submission_time", "title", "message"]
-ANSWER_HEADERS = ["id", "submission_time", "message"]
-
+HEADERS = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
+ANSWER_HEADERS = ["id", "submission_time","question_id", "message"]
 
 def get_questions_from_file():
-    questions = get_mutable_list("sample_data/question.csv")
+    questions = get_mutable_list("sample_data/test_questions.csv")
     sorted_questions = sorted(questions, key=lambda x: int(x["submission_time"]), reverse=True)
     return sorted_questions
 
 
 # util function
+# TODO: need to get rid of HEADERS condition if we extend the program
 def get_mutable_list(filename):
-    return [{key: item for key, item in dictionary.items() if key in HEADERS} 
+    return [{key: item for key, item in dictionary.items() if key in HEADERS}
             for dictionary in connection.read_data_from_file(filename)]
 
 
 def get_question_by_id(question_id):
-    questions = get_mutable_list("sample_data/question.csv")
+    questions = get_mutable_list("sample_data/test_questions.csv")
     return filter_items_by_id(question_id, questions)[0]
 
 
@@ -27,11 +28,41 @@ def filter_items_by_id(question_id, questions):
     return [question for question in questions if question["id"] == question_id]
 
 
-def get_answer_by_question_id(question_id): 
+def get_answer_by_question_id(question_id):
     answers = get_mutable_list("sample_data/answer.csv")
     return filter_items_by_id(question_id, answers)
+
+
+def add_question(form_data):
+    question_with_id_and_time = generate_question_dict(form_data)
+    questions = connection.read_data_from_file("sample_data/test_questions.csv")
+    questions.append(question_with_id_and_time)
+    connection.write_data_to_file(questions, HEADERS)
+
+
+def generate_new_id():
+    return
+
+
+def generate_question_dict(form_data):
+    question_dict = {key: value for key, value in form_data.items()}
+    add_missing_initial_values_to_question(question_dict)
+    return question_dict
+
+
+# TODO: provide id generator fn
+def add_missing_initial_values_to_question(question):
+    question['id'] = "4"
+    question["submission_time"] = int(math.ceil(time()))
+    question["image"] = question["image"] if question.get("image") else ""
+    question["view_number"] = "0"
+    question["vote_number"] = "0"
 
 
 if __name__ == "__main__":
     print(get_answer_by_question_id("2"))
     print(get_question_by_id("3"))
+    add_question({"title": "Title", "message": "This is the message"})
+    
+
+
