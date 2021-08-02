66eaaa5320910fcedcb805fb20e4816898d9c5b7
tamasstephen <kicsitnagyon@gmail.com>
2021-08-02 12:38:06 +0200

    Fix get_questions_from_file

--- data_handler.py
+++ data_handler.py
@@ -5,6 +5,6 @@ HEADERS = ["id", "submission_time", "title", "message"]
 
 
 def get_questions_from_file():
-    questions = [{key: item for key, item in dictionary.values() if key in HEADERS} 
+    questions = [{key: item for key, item in dictionary.items() if key in HEADERS} 
                   for dictionary in connection.read_data_from_file()]
     return questions
