5a8ee89e2570f25c27f55e8f43c72553223b1fb2
tamasstephen <kicsitnagyon@gmail.com>
2021-08-02 12:51:26 +0200

    Implement sorting in question data retr

--- data_handler.py
+++ data_handler.py
@@ -7,4 +7,6 @@ HEADERS = ["id", "submission_time", "title", "message"]
 def get_questions_from_file():
     questions = [{key: item for key, item in dictionary.items() if key in HEADERS} 
                   for dictionary in connection.read_data_from_file()]
-    return questions
+    sorted_questions = sorted(questions, key=lambda x: int(x["submission_time"]), reverse=True)
+    return sorted_questions
+
