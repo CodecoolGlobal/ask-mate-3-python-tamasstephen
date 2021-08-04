21e350237ebc98169bfaed7213ed924197ca58e8
matekaszas13 <matekaszas17@gmail.com>
2021-08-04 09:55:31 +0200

    delete question in progress

--- server.py
+++ server.py
@@ -11,7 +11,8 @@ def open_questions():
         questions = data_handler.get_questions_from_file(request.form["sort"])
         return render_template("index.html", questions=questions, headers=data_handler.HEADERS)
     questions = data_handler.get_questions_from_file()
-    return render_template("index.html", questions=questions, headers=data_handler.HEADERS)
+    question_id = ""
+    return render_template("index.html", questions=questions, headers=data_handler.HEADERS, question_id=question_id)
 
 
 # @app.route("/<sort>")
@@ -25,7 +26,7 @@ def open_question_page(question_id):
     question = data_handler.get_question_by_id(question_id)
     answers = data_handler.get_answers_by_question_id(question_id)
     data_handler.count_views(question_id)
-    return render_template("question.html", question=question, answers=answers)
+    return render_template("question.html", question=question, answers=answers, this_question_id=question_id)
 
 
 @app.route("/add_question", methods=["GET", "POST"])
@@ -45,8 +46,15 @@ def add_answer(question_id):
     return render_template("add_answer.html", question_id=question_id)
 
 
+@app.route("/answer/<int:question_id>/delete", methods=["POST"])
+def delete_answer(question_id):
+    print('lol')
+    data_handler.delete_questions(question_id, "sample_data/test_questions.csv")
+    return redirect("/")
+
+
 if __name__ == "__main__":
     app.run(
-        port=5000,
+        port=9000,
         debug=True
     )
