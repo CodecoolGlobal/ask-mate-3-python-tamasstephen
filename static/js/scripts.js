1103e0b4a2d3e9305168eed29d4830f51594359c
tamasstephen <kicsitnagyon@gmail.com>
2021-09-07 22:54:07 +0200

    feat: Implement feat -> when clicking outside dropdown, dropdown collapses

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,12 +3,23 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const checkboxAll = document.querySelectorAll(".check_me");
     const menuButton = document.querySelector("#menu_more");
     const menuDropdown = document.querySelector(".dropdown");
+    const dropdownAll = document.querySelectorAll(".dropdown");
 
     menuButton.addEventListener('click', (e)=>{
        menuButton.classList.toggle('open');
        menuDropdown.classList.toggle("collapsed");
     })
     checkboxAll.forEach(checkbox => checkbox.addEventListener('click', fireSubmit))
+    window.addEventListener('click', (e)=>{
+        dropdownAll.forEach((dropdown )=> {
+            const classes = Array.from(dropdown.classList);
+            console.log(e.target)
+            if(!classes.includes("collapsed") && e.target !== menuButton && e.target !== menuDropdown) {
+                dropdown.classList.add("collapsed");
+                menuButton.classList.remove("open");
+            }
+        })
+    })
 })
 
 function fireSubmit(e) {
